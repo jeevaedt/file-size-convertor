88329d074284fd6eb247ec9194cdbb0fa851b591
Jeevanantham K <jeevaedt@gmail.com>
2019-08-29 17:26:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function formatFileSize(size) {
+    var sizes = [' Bytes', ' KB', ' MB', ' GB', ' TB', ' PB', ' EB', ' ZB', ' YB'];
+    for (var i = 1; i < sizes.length; i++) {
+        if (size < Math.pow(1024, i)) return (Math.round((size / Math.pow(1024, i - 1)) * 100) / 100) + sizes[i - 1];
+    }
+    return size;
+}
+module.exports = formatFileSize;
